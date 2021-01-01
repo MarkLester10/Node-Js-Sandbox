c4f25174959533b095db121ffcf9d0b138231458
Mark Lester Morta <marklester10.mlsm@gmail.com>
2021-01-01 21:38:45 +0800

    Added Url module demo

--- Node Js Crash Course/reference/url_demo.js
+++ Node Js Crash Course/reference/url_demo.js
@@ -1,1 +1,33 @@
-// TODO: URL Module
+const url = require("url");
+
+const myUrl = new URL(
+  "http://mywebsite.com:8080/hello.html?id=100&status=active"
+);
+
+//Serialized URL
+console.log(myUrl.href);
+console.log(myUrl.toString());
+
+//Get Host name - Domain
+console.log(myUrl.host); // Port included
+
+//Get Host Name - Domain
+console.log(myUrl.hostname); //doesn't include the port
+
+//Path Name
+console.log(myUrl.pathname); //actual file
+
+//Serialized Query
+console.log(myUrl.search);
+
+//Serialized Query - Params Object
+console.log(myUrl.searchParams);
+
+//Serialized Query - Add Params
+myUrl.searchParams.append("abc", "123");
+console.log(myUrl.searchParams);
+
+//Loop through params
+myUrl.searchParams.forEach((value, name) => {
+  console.log(`${name}: ${value}`);
+});
